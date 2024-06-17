3f23d425f9719f526ecd8a5b046a637476d15767
Konrad Miekus <miekus.konrad@gmail.com>
2024-06-17 08:12:34 +0200

    test links

--- app/src/About.tsx
+++ app/src/About.tsx
@@ -2,6 +2,7 @@ export default function About() {
 
   return (
     <div>
+       <a href='/home'>Home</a>
        <h1>About</h1>
     </div>
   )
--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+import Counter from './Counter'
 
+export default function Home() {
   return (
     <div>
-       <h1>Home</h1>
+       <a href='/about'>About</a>
+       <Counter/>
     </div>
   )
 }
